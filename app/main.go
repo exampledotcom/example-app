1cbbbeaa19ac2ab87c6901ac983d27145c0040cd
Burak Kirici <burak.kirici@accenture.com>
2023-11-29 01:02:04 +0100

    No deployment for me!

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	port := ":9090"
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello!\nRequest path: %s\nRequest method: %s\n", r.URL.Path, r.Method)
+        fmt.Fprintf(w, "Goodbye!\nRequest path: %s\nRequest method: %s\n", r.URL.Path, r.Method)
     })
     fmt.Printf("Listening on %s", port)
     http.ListenAndServe(port, nil)
